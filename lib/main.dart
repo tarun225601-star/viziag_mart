ad31328ef76e5a1904011ee5e47478f3f0916922
tarun225601-star <tarun225601@gmail.com>
2026-09-13 10:45:13 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'vendor_auth_and_portal_view.dart';
+import 'vendor_auth_portal.dart';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'database_models.dart';
