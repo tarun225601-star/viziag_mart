af0ec8b0b5cb146ea1764d7469c5e8d18a22b947
tarun225601-star <tarun225601@gmail.com>
2026-09-15 15:00:26 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -932,11 +932,13 @@ class _VendorOrdersTabState extends State<VendorOrdersTab> {
                 Text('कुल राशि: ₹${ord['totalAmount'] ?? '0'}', style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14, color: Colors.green)),
                 Text('स्टेटस: ${ord['status'] ?? 'Pending'}', style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.blue)),
                         ],
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
+  }
+ }
   
 
 class VendorSettingsTab extends StatefulWidget {
